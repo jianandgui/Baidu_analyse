82ab0aaae6000bbe8490372d749a54cae2455437
muyi <yangquan95@163.com>
2017-06-13 19:57:28 +0800

    change a little

--- src/main/java/cn/edu/swpu/cins/event/analyse/platform/controller/AuthController.java
+++ src/main/java/cn/edu/swpu/cins/event/analyse/platform/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class AuthController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity createTeacherAuthenticationToken(
             @RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException, BaseException {
 
