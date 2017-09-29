2124ae1eaef371266c979b78a43af16d012abcb0
muyi <yangquan95@163.com>
2017-09-29 20:26:18 +0800

    测试jar包5

--- src/main/java/cn/edu/swpu/cins/event/analyse/platform/service/SpecialPostEventChartService.java
+++ src/main/java/cn/edu/swpu/cins/event/analyse/platform/service/SpecialPostEventChartService.java
@@ -2,12 +2,11 @@ package cn.edu.swpu.cins.event.analyse.platform.service;
 
 import cn.edu.swpu.cins.event.analyse.platform.exception.NoEventException;
 import cn.edu.swpu.cins.event.analyse.platform.model.view.ChartPoint;
-import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface SpecialPostEventChartService {
 
-    List<ChartPoint> getChartPoints(String url) throws JSONException, NoEventException, IOException;
+    List<ChartPoint> getChartPoints(String url) throws NoEventException, IOException;
 }
