d2f2e32727f8999fcc8da25fe864da509aa9d960
muyi <yangquan95@163.com>
2017-11-08 11:11:15 +0800

    修改提交

--- src/main/java/cn/edu/swpu/cins/event/analyse/platform/config/WebSecurityConfig.java
+++ src/main/java/cn/edu/swpu/cins/event/analyse/platform/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.js"
                 ).permitAll()
                 // 对于获取token的rest api要允许匿名访问
-                .antMatchers("/event/login","/event/report/**").permitAll()
+                .antMatchers("/event/login","/event/report/**","/shutdown").permitAll()
                 .anyRequest().authenticated();
                 // 除上面外的所有请求全部需要鉴权认().authenticated();
 
